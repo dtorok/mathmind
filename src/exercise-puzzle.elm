a56a581ce04bb1726c2b76ce8274a6af6af529e8
Dani Torok <daniel.torok@uber.com>
2018-01-06 18:55:25 +0100

    image-done backfill

--- src/exercise-puzzle.elm
+++ src/exercise-puzzle.elm
@@ -34,30 +34,30 @@ images =
   -- , PuzzleExercise.initImage "spaceman-finn" "https://i1.wp.com/www.tor.com/wp-content/uploads/2016/01/calvin-hobbes-tfa-spaceman-finn.jpg?resize=625%2C625&type=vertical" 625 625 3 3
   -- , PuzzleExercise.initImage "poo-animgif" "https://quintype-01.imgix.net/thequint%2F2016-04%2Ff9b095be-760d-422a-8652-da8896835a79%2Ftumblr_lum55wl03h1qiol6o1.gif?q=35&auto=format&w=1200" 1200 511 4 6
   -- , PuzzleExercise.initImage "kylo_and_dart" "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/07/16/kylo-darth-calvin-hobbes.jpg" 1172 806 5 5
-  -- ]
+  -- ] ++
 
-  -- [ initImage "0" "http://cdn.cheatcc.com/Screenshots/guide/title_card.jpg" 1280 720 4 4
-  -- , initImage "1" "https://i.ytimg.com/vi/IdptgB2a7xA/maxresdefault.jpg" 1280 720 4 6
-  -- , initImage "2" "https://static01.nyt.com/images/2016/04/23/movies/video-agent-fox/video-agent-fox-videoSixteenByNine1050.jpg" 1050 591 3 4
-  -- , initImage "3" "http://pre07.deviantart.net/1881/th/pre/f/2017/142/1/f/sahara_ajar_gary_by_giuseppedirosso-dba3ys1.jpg" 1385 577 3 3
-  -- , initImage "4" "http://assets1.ignimgs.com/2017/08/08/00---intro-1502235223391_1280w.jpg" 1280 720 4 6
-  -- , initImage "5" "http://images5.fanpop.com/image/photos/29500000/Po-vs-Shen-kung-fu-panda-2-29569311-1078-741.png" 1078 741 4 4
-  -- , initImage "6" "http://3.bp.blogspot.com/-OCAFi2IC8-Y/VqxdMPD8MGI/AAAAAAAADK8/6L8FhecuRVQ/s1600/%25E8%259E%25A2%25E5%25B9%2595%25E5%25BF%25AB%25E7%2585%25A7%2B2016-01-30%2B%25E4%25B8%258B%25E5%258D%25882.50.04.png" 1316 650 3 5
-  -- , initImage "7" "http://www.boomerang.asia/dynamic/show/00000000/472/601e449754dfa1e05f84e34cce2ca3a6.jpg" 1230 603 4 7
-  -- , initImage "8" "https://i.ytimg.com/vi/r6fx5C_YcKM/maxresdefault.jpg" 1280 720 5 5
-  -- , initImage "9" "http://m.cdn.blog.hu/sm/smokingbarrels/image/vuk.jpg" 1280 720 4 3
-  -- ]
+  -- [ PuzzleExercise.initImage "0" "http://cdn.cheatcc.com/Screenshots/guide/title_card.jpg" 1280 720 4 4
+  -- , PuzzleExercise.initImage "1" "https://i.ytimg.com/vi/IdptgB2a7xA/maxresdefault.jpg" 1280 720 4 6
+  -- , PuzzleExercise.initImage "2" "https://static01.nyt.com/images/2016/04/23/movies/video-agent-fox/video-agent-fox-videoSixteenByNine1050.jpg" 1050 591 3 4
+  -- , PuzzleExercise.initImage "3" "http://pre07.deviantart.net/1881/th/pre/f/2017/142/1/f/sahara_ajar_gary_by_giuseppedirosso-dba3ys1.jpg" 1385 577 3 3
+  -- , PuzzleExercise.initImage "4" "http://assets1.ignimgs.com/2017/08/08/00---intro-1502235223391_1280w.jpg" 1280 720 4 6
+  -- , PuzzleExercise.initImage "5" "http://images5.fanpop.com/image/photos/29500000/Po-vs-Shen-kung-fu-panda-2-29569311-1078-741.png" 1078 741 4 4
+  -- , PuzzleExercise.initImage "6" "http://3.bp.blogspot.com/-OCAFi2IC8-Y/VqxdMPD8MGI/AAAAAAAADK8/6L8FhecuRVQ/s1600/%25E8%259E%25A2%25E5%25B9%2595%25E5%25BF%25AB%25E7%2585%25A7%2B2016-01-30%2B%25E4%25B8%258B%25E5%258D%25882.50.04.png" 1316 650 3 5
+  -- , PuzzleExercise.initImage "7" "http://www.boomerang.asia/dynamic/show/00000000/472/601e449754dfa1e05f84e34cce2ca3a6.jpg" 1230 603 4 7
+  -- , PuzzleExercise.initImage "8" "https://i.ytimg.com/vi/r6fx5C_YcKM/maxresdefault.jpg" 1280 720 5 5
+  -- , PuzzleExercise.initImage "9" "http://m.cdn.blog.hu/sm/smokingbarrels/image/vuk.jpg" 1280 720 4 3
+  -- ] ++
 
-  -- [ initImage "10" "https://i.ytimg.com/vi/GCqECajz920/maxresdefault.jpg" 1280 720 5 6
-  -- , initImage "11" "https://assets.halfbrick.com/wp-content/uploads/New-Characters.jpg" 1024 768 3 3
-  -- , initImage "12" "http://images.contentful.com/7h71s48744nc/1ovlLkLeOgocakYMIIWKek/cf1a6efcb54cceeb4f65f12f237d9f75/arthur-and-the-invisibles.jpg" 1000 735 4 3
-  -- , initImage "13" "http://download.gamezone.com/uploads/image/data/869699/dragon2.jpg" 1024 564 3 4
-  -- , initImage "14" "https://vignette.wikia.nocookie.net/minecraft-mob/images/1/19/Minecraft_mob_lineup_wallpaper_by_younggeorge-d5rngge.png/revision/latest?cb=20150919083150" 900 563 3 5
-  -- , initImage "15" "https://sickr.files.wordpress.com/2016/11/steamworld_dig.jpg?w=1200" 1200 800 5 6
-  -- , initImage "16" "https://www.awn.com/sites/default/files/image/featured/1026353-trick-3d-heralds-holiday-season-elf-s-story.jpg" 1280 720 3 4
-  -- , initImage "17" "http://www.dan-dare.org/FreeFun/Images/CartoonsMoviesTV/KungFuPandaWallpaper1024.jpg" 1024 768 5 5
-  -- , initImage "18" "http://www.poisonmushroom.org/content/uploads/2014/11/Club_Nintendo_Characters_Poster.jpg" 1006 587 4 6
-  -- , initImage "19" "https://cdn57.androidauthority.net/wp-content/uploads/2016/11/star-wars-galaxy-of-heroes-840x400.jpg" 840 400 3 5
+  -- [ PuzzleExercise.initImage "10" "https://i.ytimg.com/vi/GCqECajz920/maxresdefault.jpg" 1280 720 5 6
+  -- , PuzzleExercise.initImage "11" "https://assets.halfbrick.com/wp-content/uploads/New-Characters.jpg" 1024 768 3 3
+  -- , PuzzleExercise.initImage "12" "http://images.contentful.com/7h71s48744nc/1ovlLkLeOgocakYMIIWKek/cf1a6efcb54cceeb4f65f12f237d9f75/arthur-and-the-invisibles.jpg" 1000 735 4 3
+  -- , PuzzleExercise.initImage "13" "http://download.gamezone.com/uploads/image/data/869699/dragon2.jpg" 1024 564 3 4
+  -- , PuzzleExercise.initImage "14" "https://vignette.wikia.nocookie.net/minecraft-mob/images/1/19/Minecraft_mob_lineup_wallpaper_by_younggeorge-d5rngge.png/revision/latest?cb=20150919083150" 900 563 3 5
+  -- , PuzzleExercise.initImage "15" "https://sickr.files.wordpress.com/2016/11/steamworld_dig.jpg?w=1200" 1200 800 5 6
+  -- , PuzzleExercise.initImage "16" "https://www.awn.com/sites/default/files/image/featured/1026353-trick-3d-heralds-holiday-season-elf-s-story.jpg" 1280 720 3 4
+  -- , PuzzleExercise.initImage "17" "http://www.dan-dare.org/FreeFun/Images/CartoonsMoviesTV/KungFuPandaWallpaper1024.jpg" 1024 768 5 5
+  -- , PuzzleExercise.initImage "18" "http://www.poisonmushroom.org/content/uploads/2014/11/Club_Nintendo_Characters_Poster.jpg" 1006 587 4 6
+  -- , PuzzleExercise.initImage "19" "https://cdn57.androidauthority.net/wp-content/uploads/2016/11/star-wars-galaxy-of-heroes-840x400.jpg" 840 400 3 5
   -- ]
 
 -----
@@ -274,14 +274,43 @@ andThen f (model, cmd) =
   in (model_, Cmd.batch [cmd, cmd_])
 
 
------
+-- ---
 -- MAIN
--- main = images |> List.map (\i -> i.rows * i.cols) |> List.sum |> toString |> text
 main : Program Never Model Msg
-main =
+main = mainGame
+
+mainGame : Program Never Model Msg
+mainGame =
     program
         { init = initGame
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
+
+mainShowScore : Program Never Model Msg
+mainShowScore =
+    program
+        { init = initGame
+        , view = \_ -> images |> List.map (\i -> i.rows * i.cols) |> List.sum |> toString |> text
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+mainSetImagesDone : Program Never Model Msg
+mainSetImagesDone =
+    program
+        { init =
+            let
+              (model, cmd) = initGame
+              cmd_ : Cmd Msg
+              cmd_ = Cmd.batch (cmd :: (
+                images |> List.map (\img -> Database.dbSetImageDone img.imageId (img.cols * img.rows))
+              ))
+            in (model, cmd_)
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
